f1d5e7acac29cc5ddf86d923a26f647a5fb9f7df
Dias <dias.atudinov@gmail.com>
2023-05-14 13:57:47 +0600

    Initial Commit

---
+++ Geographic atlas/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Geographic atlas
+//
+//  Created by Atudinov Dias on 14.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Geographic atlas/Geographic_atlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geographic_atlasApp.swift
+//  Geographic atlas
+//
+//  Created by Atudinov Dias on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Geographic_atlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
